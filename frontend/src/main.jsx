27e6912fa0fabda30bb41bae8fb006b5531be4e5
koppojuTrinaini <koppojutrinaini@gmail.com>
2026-05-20 12:52:40 +0530

    fixed router

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import { Toaster } from 'react-hot-toast'
+import App from './App'
 import './index.css'
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter basename="/Student-Attendence">
       <App />
-      <Toaster />
     </BrowserRouter>
-  </StrictMode>,
+  </React.StrictMode>
 )
